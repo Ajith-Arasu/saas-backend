aecf819da71c41f48111a1b364d68c835f88c787
Prakash Maria Liju P <helloprakash33@gmail.com>
2025-08-09 16:58:41 +0530

    Added order

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,20 @@
 import express from 'express';
 import { setRoutes } from './routes/index';
+import cors from 'cors';
 
 const app = express();
+app.use(cors({
+  origin: 'http://localhost:5173', // allow all origins
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true
+}));
+// app.use(function (req, res, next) {
+//     res.header("Access-Control-Allow-Origin", "*");
+//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+//     next();
+// });
+// app.options('*', cors());
 const PORT = process.env.PORT || 3000;
 
 // Middleware
